88c36e3c2f32bb96680e4e851888e37bf3f2b524
Leon <schneider.7@gmx.de>
2021-12-13 18:13:22 +0100

    added api route

--- src/frontend/server.py
+++ src/frontend/server.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
-
 port = os.environ.get("PORT")
+
+
+@app.route("/api/createFloodzone")
+def getFloodzone():
+    # /api/createFloodzone?x=12345&y=12345&r=1&h=1
+    x = request.args.get("x")
+    y = request.arg.get("y")
+    radius = request.args.get("r")
+    height = request.args.get("h")
+    return
 
 
 @app.route("/")
