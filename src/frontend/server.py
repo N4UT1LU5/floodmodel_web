07ce34aac0bc0abf0f15633bb4ad4db7dc165710
Leon <schneider.7@gmx.de>
2021-12-16 12:37:27 +0100

    edited createFloodzone route

--- src/frontend/server.py
+++ src/frontend/server.py
@@ -1,5 +1,5 @@
 import os
-from ..backend import modeling
+from backend import modeling as mdl
 
 # import src.backend.modeling as modl
 from flask import Flask, render_template, request
@@ -14,12 +14,12 @@ port = os.environ.get("PORT")
 @app.route("/api/createFloodzone")
 def getFloodzone():
     # /api/createFloodzone?x=12345&y=12345&r=1&h=1
-    x = request.args.get("x")
-    y = request.arg.get("y")
-    radius = request.args.get("r")
-    height = request.args.get("h")
-    # loc = modl.setLocation(x, y, radius)
-    return
+    x = int(request.args.get("x"))
+    y = int(request.args.get("y"))
+    radius = int(request.args.get("r"))
+    height = int(request.args.get("h"))
+    loc = (x, y, radius)
+    return mdl.createFloodzoneMultiTile(height, loc)
 
 
 @app.route("/")
