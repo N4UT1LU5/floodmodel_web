e9f213cd9b94728563b2d4d572e7f6c06cec7de9
Timo <timo-kreuels@gmx.de>
2021-12-24 00:34:05 +0100

    added slidebar

--- src/frontend/static/js/map.js
+++ src/frontend/static/js/map.js
@@ -16,121 +16,132 @@ var circle;
 var fixedRectangle;
 
 function initControls() {
-  if (mymap) {
-    // zoom control as default
+    if (mymap) {
+        // zoom control as default
 
-    // attribution control - static suffix
-    mymap.attributionControl.addAttribution("BO GI II Demo Webapp");
+        // attribution control - static suffix
+        mymap.attributionControl.addAttribution("BO GI II Demo Webapp");
 
-    // scale control
-    L.control
-      .scale({ maxWidth: 500, metric: true, imperial: false })
-      .addTo(mymap);
+        // scale control
+        L.control.scale({ maxWidth: 500, metric: true, imperial: false }).addTo(mymap);
 
-    // layerControl
-    var baseLayers = {};
-    var overlayLayers = {};
-    myLayerControl = L.control.layers(baseLayers, overlayLayers);
-    myLayerControl.addTo(mymap);
-  }
+        // layerControl
+        var baseLayers = {};
+        var overlayLayers = {};
+        myLayerControl = L.control.layers(baseLayers, overlayLayers);
+        myLayerControl.addTo(mymap);
+    }
 }
 
 function initBackgroundLayers() {
-  if (mymap && myLayerControl) {
-    // specify all background WMS layers
-    // only OSM as active layer
+    if (mymap && myLayerControl) {
+        // specify all background WMS layers
+        // only OSM as active layer
 
-    var wmsLayer_topplus = L.tileLayer.wms(
-      "http://sgx.geodatenzentrum.de/wms_topplus_web_open?",
-      {
-        format: "image/png",
-        layers: "web",
-        attribution: "&copy; Bundesamt für Kartographie und Geod&auml;sie",
-      }
-    );
+        var wmsLayer_topplus = L.tileLayer.wms("http://sgx.geodatenzentrum.de/wms_topplus_web_open?", {
+            format: "image/png",
+            layers: "web",
+            attribution: "&copy; Bundesamt für Kartographie und Geod&auml;sie"
+        });
 
-    var wmsLayer_osm = L.tileLayer
-      .wms("https://maps.heigit.org/osm-wms/service?", {
-        format: "image/png",
-        layers: "osm_auto:all",
-        attribution:
-          '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      })
-      .addTo(mymap);
+        var wmsLayer_osm = L.tileLayer
+            .wms("https://maps.heigit.org/osm-wms/service?", {
+                format: "image/png",
+                layers: "osm_auto:all",
+                attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            })
+            .addTo(mymap);
 
-    var wmsLayer_dtk = L.tileLayer.wms(
-      "https://www.wms.nrw.de/geobasis/wms_nw_dtk?",
-      {
-        format: "image/png",
-        layers: "nw_dtk_sw",
-      }
-    );
+        var wmsLayer_dtk = L.tileLayer.wms("https://www.wms.nrw.de/geobasis/wms_nw_dtk?", {
+            format: "image/png",
+            layers: "nw_dtk_sw"
+        });
 
-    // add baseLayers to Base Layers in layer control
-    myLayerControl.addBaseLayer(wmsLayer_topplus, "TopPlus WMS");
-    myLayerControl.addBaseLayer(wmsLayer_osm, "OSM WMS");
-    myLayerControl.addBaseLayer(wmsLayer_dtk, "DTK WMS");
-  }
+        // add baseLayers to Base Layers in layer control
+        myLayerControl.addBaseLayer(wmsLayer_topplus, "TopPlus WMS");
+        myLayerControl.addBaseLayer(wmsLayer_osm, "OSM WMS");
+        myLayerControl.addBaseLayer(wmsLayer_dtk, "DTK WMS");
+    }
 }
 
 function initMap() {
-  mymap = L.map(mapContainerNodeId).setView([51.461372, 7.2418863], 12);
+    mymap = L.map(mapContainerNodeId).setView([51.461372, 7.2418863], 12);
 
-  initControls();
+    initControls();
 
-  initBackgroundLayers();
+    initBackgroundLayers();
 
-  Rectangle = L.circle();
-  function onMapClick(e) {
-    lalo = e.latlng;
-    var t = document.getElementById("myRange").value * 100;
+    Rectangle = L.circle();
+    function onMapClick(e) {
+        lalo = e.latlng;
+        var t = document.getElementById("myRange").value * 100;
 
-    circle = L.circle(e.latlng, t, {
-      color: "#C02900",
-      weight: 1,
-      opacity: 0,
-      fillColor: "#ff0000",
-      fillOpacity: 0,
-    }).addTo(mymap);
-    recBounds = circle.getBounds();
+        circle = L.circle(e.latlng, t, {
+            color: "#C02900",
+            weight: 1,
+            opacity: 0,
+            fillColor: "#ff0000",
+            fillOpacity: 0
+        }).addTo(mymap);
+        recBounds = circle.getBounds();
 
-    console.log(recBounds);
+        console.log(recBounds);
 
-    console.log(recBounds);
-    L.rectangle(recBounds, {
-      color: "#0000ff",
-      weight: 5,
-      fillOpacity: 0,
-    }).addTo(mymap);
-  }
+        console.log(recBounds);
+        L.rectangle(recBounds, {
+            color: "#0000ff",
+            weight: 5,
+            fillOpacity: 0
+        }).addTo(mymap);
+    }
 
-  mymap.on("click", onMapClick);
+    mymap.on("click", onMapClick);
 }
 
 function getFloodzone(x, y, r, h) {
-  fetch(`http://127.0.0.1:5000/api/createFloodzone?x=${x}&y=${y}&r=${r}&h=${h}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.features);
-      L.geoJSON(data.features).addTo(mymap);
-    });
+    fetch(`http://127.0.0.1:5000/api/createFloodzone?x=${x}&y=${y}&r=${r}&h=${h}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.features);
+            L.geoJSON(data.features).addTo(mymap);
+        });
 }
 
 function onDomLoaded() {
-  initMap();
+    initMap();
+    // SLIDER JS Waterheight
+    var sliderWater = document.getElementById("myHeight");
+    var outputWater = document.getElementById("demowater");
+    outputWater.innerHTML = document.getElementById("myHeight").value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    sliderWater.oninput = function () {
+        outputWater.innerHTML = this.value;
+        waterheight = this.value;
+    };
+    // SLIDER JS Radius
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demoradius");
+    output.innerHTML = document.getElementById("myRange").value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function () {
+        output.innerHTML = this.value * 100;
+        radius = this.value * 100;
+    };
 }
 
 function sendRequestFloodzone() {
-  getFloodzone(x, y, r, h);
+    getFloodzone(x, y, r, h);
 }
 
 document.addEventListener("DOMContentLoaded", onDomLoaded);
 
 function fixLocation() {
-  fixedRectangle = L.rectangle(recBounds, {
-    color: "#ff0000",
-    weight: 5,
-    fillOpacity: 0,
-  });
-  fixedRectangle.addTo(mymap);
+    fixedRectangle = L.rectangle(recBounds, {
+        color: "#ff0000",
+        weight: 5,
+        fillOpacity: 0
+    });
+    fixedRectangle.addTo(mymap);
 }
