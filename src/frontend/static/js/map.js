766d1395fb3e21a443eb4015d5b4dacfe12f48d5
Leon <schneider.7@gmx.de>
2021-12-23 14:27:45 +0100

    edited map.js

--- src/frontend/static/js/map.js
+++ src/frontend/static/js/map.js
@@ -102,6 +102,15 @@ function initMap() {
     mymap.on("click", onMapClick);
 }
 
+function getFloodzone(x, y, r, h) {
+    fetch(`http://127.0.0.1:5000/api/createFloodzone?x=${x}&y=${y}&r=${r}&h=${h}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data.features);
+            L.geoJSON(data.features).addTo(mymap);
+        });
+}
+
 function onDomLoaded() {
     initMap();
 }
