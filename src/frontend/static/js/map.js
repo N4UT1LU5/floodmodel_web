b7fdfc8fc6909d290010b62e8255cfb8f0b357e9
Leon <schneider.7@gmx.de>
2021-12-27 17:59:17 +0100

    minor changes to controls

--- src/frontend/static/js/map.js
+++ src/frontend/static/js/map.js
@@ -6,14 +6,16 @@ var mymap;
 var myLayerControl;
 var x = 0;
 var y = 0;
+var x_fixed = x;
+var y_fixed = y;
 var h = 1;
 var r = 1000;
-var lalo = 0;
-var radius = 0;
+var lalo;
 var recBounds = [];
-var mymap;
 var circle;
 var RecFixed;
+var RecUnfixed;
+var floodzone_layer;
 
 function initControls() {
     if (mymap) {
@@ -37,110 +39,129 @@ function initBackgroundLayers() {
     if (mymap && myLayerControl) {
         // specify all background WMS layers
         // only OSM as active layer
-
+        //https://leaflet-extras.github.io/leaflet-providers/preview/
         var wmsLayer_topplus = L.tileLayer.wms("http://sgx.geodatenzentrum.de/wms_topplus_web_open?", {
             format: "image/png",
             layers: "web",
             attribution: "&copy; Bundesamt für Kartographie und Geod&auml;sie"
         });
 
-        var wmsLayer_osm = L.tileLayer
-            .wms("https://maps.heigit.org/osm-wms/service?", {
-                format: "image/png",
-                layers: "osm_auto:all",
-                attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            })
-            .addTo(mymap);
+        var wmsLayer_osm = L.tileLayer("https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png", {
+            maxZoom: 18,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
 
         var wmsLayer_dtk = L.tileLayer.wms("https://www.wms.nrw.de/geobasis/wms_nw_dtk?", {
             format: "image/png",
             layers: "nw_dtk_sw"
         });
+        var Esri_WorldImagery = L.tileLayer(
+            "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+            {
+                attribution:
+                    "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+            }
+        );
 
         // add baseLayers to Base Layers in layer control
+        myLayerControl.addBaseLayer(wmsLayer_osm, "OSM");
         myLayerControl.addBaseLayer(wmsLayer_topplus, "TopPlus WMS");
-        myLayerControl.addBaseLayer(wmsLayer_osm, "OSM WMS");
         myLayerControl.addBaseLayer(wmsLayer_dtk, "DTK WMS");
+        myLayerControl.addBaseLayer(Esri_WorldImagery, "Luftbild");
     }
 }
 
+function makeTileOutline() {
+    if (RecUnfixed) {
+        mymap.removeLayer(RecUnfixed);
+    }
+    circle = L.circle(lalo, r, {
+        color: "#C02900",
+        weight: 1,
+        opacity: 0,
+        fillColor: "#ff0000",
+        fillOpacity: 0
+    }).addTo(mymap);
+
+    recBounds = circle.getBounds();
+    removeNamedLayer(RecUnfixed);
+    RecUnfixed = L.rectangle(recBounds, {
+        color: "#0000ff",
+        weight: 5,
+        fillOpacity: 0
+    }).addTo(mymap);
+}
+
 function initMap() {
-    mymap = L.map(mapContainerNodeId).setView([51.461372, 7.2418863], 12);
+    mymap = L.map(mapContainerNodeId).setView([51.430887039964055, 7.27251886572458], 14);
 
     initControls();
 
     initBackgroundLayers();
-    let RecUnfixed;
-    Rectangle = L.circle();
-    function onMapClick(e) {
-        lalo = e.latlng;
-        var t = document.getElementById("myRange").value * 100;
-
-        circle = L.circle(e.latlng, t, {
-            color: "#C02900",
-            weight: 1,
-            opacity: 0,
-            fillColor: "#ff0000",
-            fillOpacity: 0
-        }).addTo(mymap);
-        recBounds = circle.getBounds();
-        // console.log(RecUnfixed);
-        if (RecUnfixed) {
-            mymap.removeLayer(RecUnfixed);
-        }
-        RecUnfixed = L.rectangle(recBounds, {
-            color: "#0000ff",
-            weight: 5,
-            fillOpacity: 0
-        }).addTo(mymap);
-    }
 
     mymap.on("click", onMapClick);
 }
 
+function onMapClick(e) {
+    lalo = e.latlng;
+    x = lalo.lat;
+    y = lalo.lng;
+    makeTileOutline();
+}
+
+function removeNamedLayer(layername) {
+    if (layername) {
+        mymap.removeLayer(layername);
+    }
+}
+
 function getFloodzone(x, y, r, h) {
+    x = parseInt(x * 100000);
+    y = parseInt(y * 100000);
     fetch(`http://127.0.0.1:5000/api/createFloodzone?x=${x}&y=${y}&r=${r}&h=${h}`)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data.features);
-            L.geoJSON(data.features).addTo(mymap);
+            // console.log(data.features);
+            removeNamedLayer(floodzone_layer);
+            floodzone_layer = L.geoJSON(data.features).addTo(mymap);
         });
 }
 
 function onDomLoaded() {
     initMap();
     // SLIDER JS Waterheight
-    var sliderWater = document.getElementById("myHeight");
-    var outputWater = document.getElementById("demowater");
-    outputWater.innerHTML = document.getElementById("myHeight").value; // Display the default slider value
+    let sliderWater = document.getElementById("height_slider");
+    let outputWater = document.getElementById("height_label_value");
+    outputWater.innerHTML = document.getElementById("height_slider").value; // Display the default slider value
 
     // Update the current slider value (each time you drag the slider handle)
     sliderWater.oninput = function () {
         outputWater.innerHTML = this.value;
-        waterheight = this.value;
+        h = this.value;
     };
     // SLIDER JS Radius
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("demoradius");
-    output.innerHTML = document.getElementById("myRange").value; // Display the default slider value
+    let sliderRadius = document.getElementById("radius_slider");
+    let outputRadius = document.getElementById("radius_label_value");
+    outputRadius.innerHTML = document.getElementById("radius_slider").value * 100; // Display the default slider value
 
     // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function () {
-        output.innerHTML = this.value * 100;
-        radius = this.value * 100;
+    sliderRadius.oninput = function () {
+        outputRadius.innerHTML = this.value * 100;
+        r = this.value * 100;
+        makeTileOutline();
     };
 }
 
 function sendRequestFloodzone() {
-    getFloodzone(x, y, r, h);
+    getFloodzone(x_fixed, y_fixed, r, h);
 }
 
-document.addEventListener("DOMContentLoaded", onDomLoaded);
-
 function fixLocation() {
-    if (RecFixed) {
-        mymap.removeLayer(RecFixed);
-    }
+    removeNamedLayer(RecFixed);
+    // set x,y values
+    x_fixed = x;
+    y_fixed = y;
+
     RecFixed = L.rectangle(recBounds, {
         color: "#ff0000",
         weight: 5,
@@ -148,3 +169,5 @@ function fixLocation() {
     });
     RecFixed.addTo(mymap);
 }
+
+document.addEventListener("DOMContentLoaded", onDomLoaded);
